95fec716e80bd1bfd65ed3435d6b460c214ccb88
Simon Shen <warsocrates@gmail.com>
2022-01-15 21:06:35 +0800

    implemented Home View

---
+++ UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Simon Shen on 1/15/22.
+//
+
+import SwiftUI
+
+extension Color {
+  static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+  static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
