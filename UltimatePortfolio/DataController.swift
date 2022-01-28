8f60df65e9a307e8d8a2ae29797e0c956fb04094
Simon Shen <warsocrates@gmail.com>
2022-01-28 08:58:01 +0800

    add tests for development data

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -20,7 +20,7 @@ class DataController: ObservableObject {
   /// Defaults to permanent storage.
   /// - Parameter inMemory: Whether to store this data in temporary memory or not.
   init(inMemory: Bool = false) {
-    container = NSPersistentCloudKitContainer(name: "Main")
+    container = NSPersistentCloudKitContainer(name: "Main", managedObjectModel: Self.model)
     
     // for testing and previewing purposes, we create a temporary,
     // in-memory database by writing to /dev/null so our data is
@@ -46,6 +46,19 @@ class DataController: ObservableObject {
     }
     
     return dataController
+  }()
+  
+  // load once and cache it
+  static let model: NSManagedObjectModel = {
+    guard let url = Bundle.main.url(forResource: "Main", withExtension: "momd") else {
+      fatalError("Failed to locate model file.")
+    }
+    
+    guard let managedObjectModel = NSManagedObjectModel(contentsOf: url) else {
+      fatalError("Failed to load model file.")
+    }
+    
+    return managedObjectModel
   }()
   
   /// Create example projects and items to make manual testing easier.
